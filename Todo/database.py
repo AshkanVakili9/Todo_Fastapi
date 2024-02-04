34f43eb9199e805fc834c895673b9860a7755fe0
Ashkan <92475017+AshkanVakili9@users.noreply.github.com>
2024-02-04 22:12:37 +0330

    nc

--- Todo/database.py
+++ Todo/database.py
@@ -4,7 +4,8 @@ from sqlalchemy.ext.declarative import declarative_base
 
 
 
-DATABASE_URL = 'postgresql://postgres:araa23o385@localhost/TodoApplicationDatabase'
+# DATABASE_URL = 'postgresql://postgres:araa23o385@localhost/TodoApplicationDatabase'
+DATABASE_URL = "sqlite:///./test.db"
 
 
 engine = create_engine(
